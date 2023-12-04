9fc4324b8f1efc7c8014fadc5fb11f1b65324cc9
Chuka Nwobi <chuka@squareup.com>
2023-12-03 23:04:55 -0500

    Working solution

--- src/day3/Day03.kt
+++ src/day3/Day03.kt
@@ -4,25 +4,29 @@ import println
 import readInput
 
 
-data class Index(val row:Int, val col: Int)
-data class PartNumber(val start:Index, val value:Int)
-data class PartNumberStr(val start:Index, val value:String)
+data class Index(val row: Int, val col: Int)
+data class PartNumber(val start: Index, val value: Int)
+data class PartNumberStr(val start: Index, val value: String)
+
 fun main() {
     val indexToNumber = mutableMapOf<Index, PartNumber>()
+    val delta = listOf(-1, 0, 1)
+    val neighbourIndexDelta = delta.flatMap { d -> delta.map { d2 -> Index(d, d2) } }.filter { it != Index(0,0) }
 
-    fun populateReverseIndex(row:Int,col:Int,numStr:String) {
-        val coordinateStart = Index(row,col)
-        val actualNum = PartNumber(coordinateStart,numStr.toInt())
-
+    fun populateReverseIndex(row: Int, col: Int, numStr: String) {
+        val coordinateStart = Index(row, col)
+        val actualNum = PartNumber(coordinateStart, numStr.toInt())
         for (idx in numStr.indices) {
-            indexToNumber[Index(row,col+idx) ] = actualNum
+            indexToNumber[Index(row, col + idx)] = actualNum
         }
     }
 
-    fun processLine(row: Int, line:String) {
+    fun processLine(row: Int, line: String) {
         var start = 0
         var inDigit = false
-
+        fun processNum(end: Int) {
+            populateReverseIndex(row, start, line.substring(start, end))
+        }
         for (index in line.indices) {
             if (line[index].isDigit() && !inDigit) {
                 start = index
@@ -30,37 +34,25 @@ fun main() {
             }
             if (!line[index].isDigit() && inDigit) {
                 inDigit = false
-                val numStr = line.substring(start, index)
-                numStr.println()
-                populateReverseIndex(row,start,numStr)
+                processNum(index)
             }
         }
-//        indexToNumber.println()
+        if (inDigit) {
+            processNum(line.length)
+        }
 
     }
 
     fun part1(input: List<String>): Int {
-        input.forEachIndexed { row, line -> processLine(row,line) }
+        input.forEachIndexed { row, line -> processLine(row, line) }
         val partNumbers = mutableSetOf<PartNumber>()
 
-        val delta = listOf(-1,0,1)
-        for (i in delta) {
-            for (j in delta) {
-                if(!(i == 0 && j == 0)) {
-                    Pair(i,j).println()
-                }
-
-            }
-        }
-
-        input.forEachIndexed{
-            r, line ->
-            line.forEachIndexed{
-                c, ch ->
-                if(!ch.isDigit() && ch != '.') {
-                    for (i in delta) {
-                        for (j in delta) {
-                            val neighbour = Index(r+i,c+j)
+        input.forEachIndexed { r, line ->
+            line.forEachIndexed { c, ch ->
+                if (!ch.isDigit() && ch != '.') {
+                    for (i in listOf(-1, 0, 1)) {
+                        for (j in listOf(-1, 0, 1)) {
+                            val neighbour = Index(r + i, c + j)
                             indexToNumber[neighbour]?.let { partNumbers.add(it) }
                         }
                     }
@@ -72,16 +64,25 @@ fun main() {
         return partNumbers.sumOf { p -> p.value }
     }
 
+    fun getAdjacentNumbers(delta: List<Int>, it: Pair<Index, Char>) =
+            delta.flatMap { i ->
+                delta.map { j ->
+                    val (index, _) = it
+                    val neighbour = index.copy(row = index.row + i, col = index.col + j)
+                    indexToNumber[neighbour]
+                }
+            }.filterNotNull().toSet()
+
     fun part2(input: List<String>): Int {
-
-
+        val delta = listOf(-1, 0, 1)
+        input.forEachIndexed { row, line -> processLine(row, line) }
+        input.flatMapIndexed { row, line -> line.mapIndexed { col, ch -> Pair(Index(row, col), ch) } }.asSequence().filter { it.second == '*' }
+                .map { getAdjacentNumbers(delta, it) }.filter { it.size == 2 }.map { adjacentNumbers -> adjacentNumbers.fold(1) { acc, partNumber -> acc * partNumber.value } }.sum()
         return input.size
     }
 
 
-
     val input = readInput("day3/Day03")
     part1(input).println()
-//    part2(input).println()
-//    processLine(0,".........640....................724.........63*844...........*...234.690.48....*.....+........612..............*.......483.............$....")
+    part2(input).println()
 }
