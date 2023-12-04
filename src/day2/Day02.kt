f921c9dd7006c2155a27b32d702d94299936c036
Chuka Nwobi <chuka@squareup.com>
2023-12-04 08:31:02 -0500

    Clean up day 2

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -3,44 +3,27 @@ package day2
 
 import println
 import readInput
-import kotlin.math.max
 
-data class Game(val number: Int, val rolls: List<List<Pair<Int, String>>>)
+data class Cube(val count: Int, val color: String)
+data class Game(val id: Int, val cubes: List<Cube>) {
+    private val colorByCountInBag = mapOf("red" to 12, "green" to 13, "blue" to 14)
+    fun isValid():Boolean = cubes.all { (count, color) -> count <= colorByCountInBag[color]!! }
+    private fun maxPerColor(): Map<String, Int> = cubes.groupBy { it.color }.mapValues { (_, v) -> v.maxOf { it.count } }
+    fun powerOfCubes():Int = maxPerColor().values.reduce{acc,maxCount -> acc * maxCount}
+}
+
+fun lineToGame(line: String): Game {
+    val gameMatch = Regex("Game (\\d+):.+")
+    val gameNumber = gameMatch.find(line)!!.groupValues[1].toInt()
+    val match = Regex("(\\d+) (\\w+)")
+    val rolls = match.findAll(line).map { Cube(it.groupValues[1].toInt(), it.groupValues[2]) }.toList()
+    return Game(gameNumber, rolls)
+}
 
 fun main() {
-    val cubes = mapOf("red" to 12, "green" to 13, "blue" to 14)
-    fun lineToGame(line: String): Game {
-        val tokens = line.split(":")
-        val gameNumber = tokens[0].split(" ")[1].trim().toInt()
-        val rolls = tokens[1].split(";").map {
-            it.split(",").map { t ->
-                val pair = t.trim().split(" ")
-                Pair(pair[0].trim().toInt(), pair[1].trim())
-            }
-        }
-       return Game(gameNumber, rolls)
-    }
-
-    fun isValidRoll(rolls: List<Pair<Int, String>>) = rolls.map { roll -> roll.first <= cubes[roll.second]!! }
-
-    fun part1(input: List<String>): Int {
-        return input.map { lineToGame(it) }.filter { game ->
-            game.rolls.flatMap { isValidRoll(it) }.fold(true) { b, acc -> acc && b }
-        }.sumOf { game -> game.number }
-    }
-
-    fun part2(input: List<String>): Int {
-       return input.map { lineToGame(it) }.sumOf {
-            it.rolls.flatten().foldRight(mutableMapOf<String,Int>()) { (number, color), acc ->
-                acc[color] = max(number, acc.getOrDefault(color,0))
-                acc
-            }.values.reduce { acc, i -> acc * i }
-        }
-    }
-
-
+    fun part1(input: List<String>): Int = input.map { lineToGame(it) }.filter { it.isValid() }.sumOf { it.id }
+    fun part2(input: List<String>): Int = input.map { lineToGame(it) }.sumOf { it.powerOfCubes() }
     val input = readInput("day2/Day02")
     part1(input).println()
     part2(input).println()
-
 }
