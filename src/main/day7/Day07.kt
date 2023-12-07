26d62fd501594657f1e83c1af5fbbd84ffc134cf
Chuka Nwobi <chuka@squareup.com>
2023-12-07 10:11:38 -0500

    Refactor day 07

--- src/main/day7/Day07.kt
+++ src/main/day7/Day07.kt
@@ -5,134 +5,87 @@ import main.readInput
 import java.lang.Exception
 import java.lang.IllegalArgumentException
 
-enum class HandType(val value: Int) {
-    FIVE_OF_KIND(7),
-    FOUR_OF_KIND(6),
-    FULL_HOUSE(5),
-    THREE_OF_KIND(4),
-    TWO_PAIR(3),
-    ONE_PAIR(2),
-    HIGH_CARD(1)
+enum class Type(val value: Int) {
+    FIVE_OF_KIND(7), FOUR_OF_KIND(6), FULL_HOUSE(5), THREE_OF_KIND(4), TWO_PAIR(3), ONE_PAIR(2), HIGH_CARD(1)
 }
 
-data class CamelCard(val hands: List<String>, val bid: Long= 0) {
-    private val rankByHand: MutableMap<String, Int> = mutableMapOf("A" to 50,
+data class CamelCard(val cards: List<String>, val bid: Long = 0) {
+    private val cardToValue: MutableMap<String, Int> = mutableMapOf("A" to 50,
             "K" to 40, "Q" to 30, "J" to 20, "T" to 10)
-    private val rankByHand2: MutableMap<String, Int> = mutableMapOf("A" to 50,
-            "K" to 40, "Q" to 30,  "T" to 10,"J" to 1)
-    private val count = hands.groupBy { it }.mapValues { it.value.size }
-    private fun rank1(): HandType {
-        val keys = count.keys.toList()
-        if (keys.size == 5) return HandType.HIGH_CARD
-        if (count.keys.size == 1) return HandType.FIVE_OF_KIND// 5 of a kind
-        if (keys.size == 2 && count.values.any { it == 4 }) return HandType.FOUR_OF_KIND // 4 of a kind
-        if (keys.size == 2 && count.values.any { it == 3 }) return HandType.FULL_HOUSE // full house
-        if (keys.size == 3 && count.values.any { it == 3 }) return HandType.THREE_OF_KIND // three of a kind T -> 3 , 9 -> 1, 8 -> 1
-        if (count.values.count { it == 2 } == 2) return HandType.TWO_PAIR// two pair       2 -> 2 3 -> 2 4 -> 1
-        if (count.values.count { it == 2 } == 1) return HandType.ONE_PAIR
+    private val cardToValueWildCard: MutableMap<String, Int> = mutableMapOf("A" to 50,
+            "K" to 40, "Q" to 30, "T" to 10, "J" to 1)
+    private val count = cards.groupingBy { it }.eachCount()
+    private fun rank(): Type {
+        val uniqueCards = count.keys.toList().size
+        if (uniqueCards == 5) return Type.HIGH_CARD
+        if (uniqueCards == 1) return Type.FIVE_OF_KIND
+        if (uniqueCards == 2 && count.values.any { it == 4 }) return Type.FOUR_OF_KIND
+        if (uniqueCards == 2 && count.values.any { it == 3 }) return Type.FULL_HOUSE
+        if (uniqueCards == 3 && count.values.any { it == 3 }) return Type.THREE_OF_KIND
+        if (count.values.count { it == 2 } == 2) return Type.TWO_PAIR
+        if (count.values.count { it == 2 } == 1) return Type.ONE_PAIR
         throw IllegalArgumentException("Unknown card")
     }
 
-     fun rank2(): HandType {
-        if (!count.containsKey("J")) {
-            return rank1()
-        }
-        val jCount = count["J"]!!
-        if (jCount == 5 || jCount == 4) return HandType.FIVE_OF_KIND
+    fun rankWithWildCard(): Type {
+        if (!count.containsKey("J")) return rank()
 
-        val noJCount = count - "J"
-        if (jCount == 3 && noJCount.values.all { it == 2 }) return HandType.FIVE_OF_KIND
-        if (jCount == 3 && noJCount.values.all { it == 1 }) return HandType.FOUR_OF_KIND
+        val wildcards = count["J"]!!
+        if (wildcards == 5 || wildcards == 4) return Type.FIVE_OF_KIND
 
-        if (jCount == 2 && noJCount.values.all { it == 1 }) return HandType.THREE_OF_KIND
-        if (jCount == 2 && noJCount.values.any { it == 2 }) return HandType.FOUR_OF_KIND
-        if (jCount == 2 && noJCount.values.all { it == 3 }) return HandType.FIVE_OF_KIND
+        val cardCount = count - "J"
+        if (wildcards == 3 && cardCount.values.all { it == 2 }) return Type.FIVE_OF_KIND
+        if (wildcards == 3 && cardCount.values.all { it == 1 }) return Type.FOUR_OF_KIND
 
-        if (jCount == 1 && noJCount.values.all { it == 1 }) return HandType.ONE_PAIR
+        if (wildcards == 2 && cardCount.values.all { it == 1 }) return Type.THREE_OF_KIND
+        if (wildcards == 2 && cardCount.values.any { it == 2 }) return Type.FOUR_OF_KIND
+        if (wildcards == 2 && cardCount.values.all { it == 3 }) return Type.FIVE_OF_KIND
 
-        if (jCount == 1 && noJCount.values.all { it == 2 }) return HandType.FULL_HOUSE
+        if (wildcards == 1 && cardCount.values.all { it == 1 }) return Type.ONE_PAIR
 
-        if (jCount == 1 && noJCount.values.any { it == 2 }) return HandType.THREE_OF_KIND
+        if (wildcards == 1 && cardCount.values.all { it == 2 }) return Type.FULL_HOUSE
 
-        if (jCount == 1 && noJCount.values.any { it == 3 }) return HandType.FOUR_OF_KIND
-        if (jCount == 1 && noJCount.values.all { it == 4 }) return HandType.FIVE_OF_KIND
+        if (wildcards == 1 && cardCount.values.any { it == 2 }) return Type.THREE_OF_KIND
 
-        throw IllegalArgumentException("Unknown J Configuration $hands")
+        if (wildcards == 1 && cardCount.values.any { it == 3 }) return Type.FOUR_OF_KIND
+        if (wildcards == 1 && cardCount.values.all { it == 4 }) return Type.FIVE_OF_KIND
+
+        throw IllegalArgumentException("Unknown J Configuration $cards")
     }
 
     fun compare(other: CamelCard): Int {
-        if (this.rank1().value > other.rank1().value) {
-            return 1
-        }
-        if (this.rank1().value < other.rank1().value) {
-            return -1
-        }
-        return tieBreak(other)
+        if (this.rank().value == other.rank().value) return tieBreak(other, cardToValue)
+        return if (this.rank().value > other.rank().value) 1 else -1
     }
 
-    fun compare2(other: CamelCard): Int {
-        if (this.rank2().value > other.rank2().value) {
-            return 1
-        }
-        if (this.rank2().value < other.rank2().value) {
-            return -1
-        }
-        return tieBreak2(other)
+    fun compareWithWildcard(other: CamelCard): Int {
+        if (this.rankWithWildCard().value == other.rankWithWildCard().value) return tieBreak(other, cardToValueWildCard)
+        return if (this.rankWithWildCard().value > other.rankWithWildCard().value) 1 else -1
     }
 
+    private fun value(idx: Int, cardToValue: Map<String, Int>) = cardToValue[cards[idx]]
+            ?: cards[idx].toInt()
 
-
-    private fun handWorthByIdx(idx: Int): Int {
-        val h = hands[idx]
-        return rankByHand[h] ?: h.toInt()
-    }
-
-    private fun handWorthByIdx2(idx: Int): Int {
-        val h = hands[idx]
-        return rankByHand2[h] ?: h.toInt()
-    }
-
-    private fun tieBreak(other: CamelCard): Int {
-        for (idx in other.hands.indices) {
-            val thisHand = handWorthByIdx(idx)
-            val otherHand = other.handWorthByIdx(idx)
-            if (thisHand == otherHand) continue
-            if (thisHand > otherHand) return 1
-            return -1
-        }
-        throw Exception("Impossible state")
-    }
-
-    private fun tieBreak2(other: CamelCard): Int {
-        for (idx in other.hands.indices) {
-            val thisHand = handWorthByIdx2(idx)
-            val otherHand = other.handWorthByIdx2(idx)
-            if (thisHand == otherHand) continue
-            if (thisHand > otherHand) return 1
-            return -1
+    private fun tieBreak(other: CamelCard, cardToValue: Map<String, Int>): Int {
+        for (idx in other.cards.indices) {
+            if (value(idx, cardToValue) == other.value(idx, cardToValue)) continue
+            return if (value(idx, cardToValue) > other.value(idx, cardToValue)) 1 else -1
         }
         throw Exception("Impossible state")
     }
 }
 
+fun camelCards(input: List<String>) = input.map { line ->
+    val (hands, bid) = line.split(Regex("\\s+"))
+    CamelCard(hands.trim().split("").map { it.trim() }.filter { it.isNotBlank() }, bid.toLong())
+}
+
+fun scoreCards(input: List<String>, comparator: Comparator<CamelCard>) =
+        camelCards(input).sortedWith(comparator).mapIndexed { idx, card -> (idx + 1) * card.bid }.sum()
+
 fun main() {
-    fun camelCards(input: List<String>) = input.map { line ->
-        val (hands, bid) = line.split(Regex("\\s+"))
-        CamelCard(hands.trim().split("").map { it.trim() }.filter { it.isNotBlank() }, bid.toLong())
-    }
-
-    fun part1(input: List<String>): Long {
-        val cards = camelCards(input)
-        val cardComparator = Comparator<CamelCard> { o1, o2 -> o1.compare(o2) }
-        return cards.sortedWith(cardComparator).mapIndexed { idx, card -> (idx + 1) * card.bid }.sum()
-    }
-
-    fun part2(input: List<String>): Long  {
-        val cards = camelCards(input)
-        val cardComparator = Comparator<CamelCard> { o1, o2 -> o1.compare2(o2) }
-        return cards.sortedWith(cardComparator).mapIndexed { idx, card -> (idx + 1) * card.bid }.sum()
-    }
-
+    fun part1(input: List<String>) = scoreCards(input) { o1, o2 -> o1.compare(o2) }
+    fun part2(input: List<String>) = scoreCards(input) { o1, o2 -> o1.compareWithWildcard(o2) }
     val input = readInput("day7/day07_buck")
     part1(input).println()
     part2(input).println()
--- src/test/day7/CamelCardTest.kt
+++ src/test/day7/CamelCardTest.kt
@@ -1,34 +1,33 @@
 package day7
 
 import main.day7.CamelCard
-import main.day7.HandType
+import main.day7.Type
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
 import kotlin.test.expect
 
 class CamelCardTest {
 
     @Test
     fun rank2() {
-        expect(HandType.FIVE_OF_KIND) { CamelCard(listOf("J", "J", "J", "J", "J")).rank2() }
-        expect(HandType.FIVE_OF_KIND) { CamelCard(listOf("A", "A", "A", "A", "A")).rank2() }
+        expect(Type.FIVE_OF_KIND) { CamelCard(listOf("J", "J", "J", "J", "J")).rankWithWildCard() }
+        expect(Type.FIVE_OF_KIND) { CamelCard(listOf("A", "A", "A", "A", "A")).rankWithWildCard() }
 
-        expect(HandType.FIVE_OF_KIND) { CamelCard(listOf("J", "J", "J", "J", "2")).rank2() }
-        expect(HandType.FIVE_OF_KIND) { CamelCard(listOf("J", "J", "J", "2", "2")).rank2() }
-        expect(HandType.FOUR_OF_KIND) { CamelCard(listOf("J", "J", "J", "1", "2")).rank2() }
-
-
-        expect(HandType.THREE_OF_KIND) { CamelCard(listOf("J", "J", "3", "1", "2")).rank2() }
-        expect(HandType.FOUR_OF_KIND) { CamelCard(listOf("J", "J", "3", "3", "2")).rank2() }
-        expect(HandType.FIVE_OF_KIND) { CamelCard(listOf("J", "J", "3", "3", "3")).rank2() }
+        expect(Type.FIVE_OF_KIND) { CamelCard(listOf("J", "J", "J", "J", "2")).rankWithWildCard() }
+        expect(Type.FIVE_OF_KIND) { CamelCard(listOf("J", "J", "J", "2", "2")).rankWithWildCard() }
+        expect(Type.FOUR_OF_KIND) { CamelCard(listOf("J", "J", "J", "1", "2")).rankWithWildCard() }
 
 
+        expect(Type.THREE_OF_KIND) { CamelCard(listOf("J", "J", "3", "1", "2")).rankWithWildCard() }
+        expect(Type.FOUR_OF_KIND) { CamelCard(listOf("J", "J", "3", "3", "2")).rankWithWildCard() }
+        expect(Type.FIVE_OF_KIND) { CamelCard(listOf("J", "J", "3", "3", "3")).rankWithWildCard() }
 
-        expect(HandType.ONE_PAIR) { CamelCard(listOf("J", "1", "2", "3", "4")).rank2() }
-        expect(HandType.THREE_OF_KIND) { CamelCard(listOf("J", "1", "1", "3", "4")).rank2() }
-        expect(HandType.FULL_HOUSE) { CamelCard(listOf("J", "1", "1", "3", "3")).rank2() }
-        expect(HandType.FOUR_OF_KIND) { CamelCard(listOf("J", "1", "1", "1", "4")).rank2() }
-        expect(HandType.FIVE_OF_KIND) { CamelCard(listOf("J", "1", "1", "1", "1")).rank2() }
+
+
+        expect(Type.ONE_PAIR) { CamelCard(listOf("J", "1", "2", "3", "4")).rankWithWildCard() }
+        expect(Type.THREE_OF_KIND) { CamelCard(listOf("J", "1", "1", "3", "4")).rankWithWildCard() }
+        expect(Type.FULL_HOUSE) { CamelCard(listOf("J", "1", "1", "3", "3")).rankWithWildCard() }
+        expect(Type.FOUR_OF_KIND) { CamelCard(listOf("J", "1", "1", "1", "4")).rankWithWildCard() }
+        expect(Type.FIVE_OF_KIND) { CamelCard(listOf("J", "1", "1", "1", "1")).rankWithWildCard() }
     }
 }
