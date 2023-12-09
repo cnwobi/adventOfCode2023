0fa0ff883a13b8026ecd5f85fe92f96fe05c8d07
Chuka Nwobi <chuka@squareup.com>
2023-12-09 15:58:26 -0500

    Sample day

---
+++ src/main/aoc2019/SampleDay.kt
@@ -0,0 +1,24 @@
+package main.aoc2023.aoc2019
+
+import main.aoc2023.lcm
+import main.aoc2023.println
+import main.aoc2023.readInput
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+
+    fun part2(input: List<String>): Long {
+        return 0
+    }
+
+    val testInput = readInput("aoc2019/day/sample2")
+    check(part1(testInput) == 6)
+    val input = readInput("aoc2019/day8/day08")
+    part1(input).println()
+    part2(input).println()
+}
+
