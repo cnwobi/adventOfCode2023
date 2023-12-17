7487f316ba94a255e8f42e95d72b14517cd1b608
Chuka Nwobi <chuka@squareup.com>
2023-12-17 00:00:29 -0500

    Clean up day 13 part 1

--- src/main/aoc2023/day13/day13.kt
+++ src/main/aoc2023/day13/day13.kt
@@ -3,76 +3,51 @@ package aoc2023.day13
 import main.aoc2023.println
 import main.aoc2023.readInput
 
-fun grids(input:List<String>):List<List<String>> {
-    return input.fold(mutableListOf(mutableListOf<String>())){
-            acc, line ->
+
+fun toGrids(input: List<String>): List<List<String>> {
+    return input.fold(mutableListOf(mutableListOf<String>())) { acc, line ->
         if (line.isBlank()) acc.add(mutableListOf()) else acc.last().add(line)
         acc
     }
 }
 
-fun findSumOfMirrorLines(grid: List<String>):Set<Int> {
-    val horizontalLines = mutableListOf<Int>()
-    for(idx in (0 until grid.size - 1)) {
-        if (checkIfMirrored(idx, grid,0)) horizontalLines.add(idx+1)
-
-    }
-    return horizontalLines.toSet()
-}
-
-fun findSumOfMirrorLines2(grid: List<String>):Set<Int> {
-    val horizontalLines = mutableListOf<Int>()
-    for(idx in (0 until grid.size - 1)) {
-        if (checkIfMirrored(idx, grid,1)) horizontalLines.add(idx+1)
-    }
-    return horizontalLines.toSet()
-}
-
-
-
-fun gridTranspose(grid: List<String>):List<String> {
-   return  grid[0].indices.map {col ->
-        grid.indices.map { row  -> grid[row][col]}.joinToString(separator = "")
-    }
-
-}
-
-fun findDiff(line: String, line1: String,diff:Int): Boolean {
-    val c = line.zip(line1).mapIndexed { idx, v -> Triple(idx, v.first, v.second) }
-        .filter { it.second != it.third }.map { it.first }
-    return c.size <= diff
-}
-private fun checkIfMirrored(idx: Int, grid: List<String>,diff: Int): Boolean {
-    var up = idx
-    var down = idx + 1
-    var isMirror = true
-    while (up >= 0 && down < grid.size) {
-        isMirror = findDiff(grid[up],grid[down],diff)
-        if (!isMirror) break
+fun checkIsMirror(index: Int, grid: List<String>): Boolean {
+    var up = index - 1
+    var down = index + 2
+    while (up in grid.indices && down in grid.indices) {
+        if (grid[up] != grid[down]) return false
         up--
         down++
     }
-    return isMirror
+    return true
 }
+
+fun findMirrorLine(grid: List<String>): Int? {
+    for (idx in 0 until grid.size - 1) {
+         if (grid[idx] == grid[idx+1] && checkIsMirror(idx,grid)) return idx + 1
+    }
+    return null
+}
+
+fun calculateMirrorLines(grid: List<String>):Int {
+    return findMirrorLine(grid)?.let { it * 100 } ?: findMirrorLine(transpose(grid)) ?: 0
+}
+
+fun transpose(grid: List<String>): List<String> {
+   return (0 until grid[0].length).map { col ->
+        grid.indices.map { row -> grid[row][col] }.joinToString(separator = "")
+    }
+}
+
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return  grids(input).sumOf {
-            findSumOfMirrorLines(it).sum() *100 + findSumOfMirrorLines(
-                gridTranspose(
-                    it
-                )
-            ).sum()
-        }
+        val grids = toGrids(input)
+        return grids.sumOf { calculateMirrorLines(it) }
     }
 
     fun part2(input: List<String>): Int {
-
-        return  grids(input).sumOf {
-            (findSumOfMirrorLines2(it) - findSumOfMirrorLines(it)).sum() *100 +
-                    (findSumOfMirrorLines2(gridTranspose(it)) - findSumOfMirrorLines(gridTranspose(it)).sum()
-            ).sum()
-        }
+        return 0
     }
 
     val input = readInput("aoc2023/day13/day13")
