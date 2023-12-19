396947b2230863da449fe13957eb50ed1683b0b9
Chuka Nwobi <chuka@squareup.com>
2023-12-18 19:34:25 -0500

    day 18

--- src/main/aoc2023/day18/day18.kt
+++ src/main/aoc2023/day18/day18.kt
@@ -2,28 +2,42 @@ package main.aoc2023.aoc2023.day18
 
 import main.aoc2023.println
 import main.aoc2023.readInput
-import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.roundToLong
 
 fun main() {
+    val hexMap = mapOf("a" to 10L, "b" to 11, "c" to 12, "d" to 13, "e" to 14, "f" to 15)
 
-    fun toInstruction(line: String): Pair<String, Int> {
-        val (dir, steps, _) = line.split(Regex("\\s+"))
-        return Pair(dir, steps.toInt())
-    }
-
-    fun groupWalls(
-        walls: MutableList<MutableList<Pair<Int, Int>>>,
-        element: Pair<Int, Int>
-    ): MutableList<MutableList<Pair<Int, Int>>> {
-        val last = walls.lastOrNull()?.lastOrNull()
-        if (last == null || abs(element.second - last.second) > 1) {
-            walls.add(mutableListOf())
+    fun toDir(code: Char): String {
+//        0 means R, 1 means D, 2 means L, and 3 means U
+        return when (code) {
+            '0' -> "R"
+            '1' -> "D"
+            '2' -> "L"
+            '3' -> "U"
+            else -> throw IllegalStateException()
         }
-        walls.last().add(element)
-        return walls
     }
 
-    fun toDelta(dir: String, step: Int, last: Pair<Int, Int>): Pair<Int, Int> {
+    fun hexToLong(hex: String): Long {
+        return hex.reversed().mapIndexed { index, c ->
+            (hexMap[c.toString()] ?: c.toString().toLong()) * 16.0.pow(index) }
+            .sum().roundToLong()
+    }
+
+    fun toInstructionHex(line: String): Pair<String, Long> {
+        val (_,codedInstruction) = line.split(Regex("#"))
+        val hexDirCode = codedInstruction.dropLast(1)
+        val dir = toDir(hexDirCode.last())
+        return Pair(dir, hexToLong(hexDirCode.dropLast(1)))
+    }
+
+    fun toInstruction(line: String): Pair<String, Long> {
+        val (dir, steps, _) = line.split(Regex("\\s+"))
+        return Pair(dir, steps.toLong())
+    }
+
+    fun toDelta(dir: String, step: Long, last: Pair<Long, Long>): Pair<Long, Long> {
         return when (dir.trim()) {
             "R" -> last.copy(second = last.second + step)
             "L" -> last.copy(second = last.second - step)
@@ -33,63 +47,37 @@ fun main() {
         }
     }
 
-    fun drawInstruction(
-        shapeSoFar: List<Pair<Int, Int>>,
-        instruction: Pair<String, Int>
-    ): List<Pair<Int, Int>> {
+    fun buildPolygon(
+        shapeSoFar: List<Pair<Long, Long>>,
+        instruction: Pair<String, Long>
+    ): List<Pair<Long, Long>> {
         val (dir, steps) = instruction
-        val last = shapeSoFar.lastOrNull() ?: Pair(0, 0)
-        return shapeSoFar + (1..steps).map { step -> toDelta(dir, step, last) }
+        val last = shapeSoFar.lastOrNull() ?: Pair(0L, 0L)
+        return shapeSoFar + toDelta(dir, steps, last)
     }
 
-    fun part1(input: List<String>): Int {
-     val c =   input.map { toInstruction(it) }
-            .fold(listOf<Pair<Int, Int>>()) { acc, instruction ->
-                drawInstruction(acc, instruction)
-            }.groupBy { it.first }.mapValues { (k, v) ->
-                v.sortedBy { it.second }
-                    .fold(mutableListOf<MutableList<Pair<Int, Int>>>()) { acc, element ->
-                        groupWalls(
-                            acc,
-                            element
-                        )
-                    }
-
-
+    fun shoeLacePicksTheorem(input:List<String>,op:(String) -> Pair<String,Long>): Long {
+        val perimeter = input.sumOf { op(it).second }
+        val polygonPoints = input.map { op(it) }
+            .fold(listOf<Pair<Long, Long>>()) { acc, instruction ->
+                buildPolygon(acc, instruction)
             }
-                .mapValues { (k,v) ->
-                    val minY = v.first().first().second
-               val walls =  v.map {
-                    it.first().second - minY .. it.last().second - minY }
+        val closedPolygon =  (polygonPoints + polygonPoints.first())
+        val area = closedPolygon.zipWithNext().sumOf { (p1, p2) ->
+            val (x1, y1) = p1
+            val (x2, y2) = p2
+            (y1 + y2) * (x2 - x1)
+        }
 
-
-               val countOfWalls = walls.sumOf { it.toList().size }
-
-                    var seenWalls = 0
-                 val inside =   walls.zipWithNext().map { (wall1,wall2) ->
-                        seenWalls += 1
-                        (wall1.last+ 1 until wall2.first).map {
-                            if(seenWalls % 2 == 1) 1 else 0
-                        }.sum()
-                    }.sum()
-
-                    "walls $countOfWalls".println()
-                    "inside $inside".println()
-                    "\n".println()
-
-                    inside.println()
-               countOfWalls + inside
-
-                }.values.sum().println()
-//                .forEach { it.println() }
-
-
-
-        return 0
+        return (area + perimeter + 2).div(2)
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>): Long {
+        return shoeLacePicksTheorem(input,::toInstruction)
+    }
+
+    fun part2(input: List<String>): Long {
+        return shoeLacePicksTheorem(input,::toInstructionHex)
     }
 
     /*
@@ -104,7 +92,6 @@ fun main() {
 .#....#
 .######
 */
-
     val input = readInput("aoc2023/day18/day18")
     part1(input).println()
     part2(input).println()
